1d5438928daae743e78d23ca487ff0df683540d3
SINGH Nitin <nitin.singh2@soprasteria.com>
2026-04-28 13:23:46 +0530

    still in progress

--- src/Features/Auth/Screens/ForgotPassword/Forgot.tsx
+++ src/Features/Auth/Screens/ForgotPassword/Forgot.tsx
@@ -1,10 +1,14 @@
 import {useState, useEffect} from "react"
-import { Container } from "./Forgot.style"
+import { Container,Button } from "./Forgot.style"
+import InputField from "../../components/InputFileds/EmailInput"
 
 
 
 const ForgotPasswordPage =()=>{
     const [email, setEmail] = useState("")
+    const [newPassword, setNewPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [isEmailConfirmed, setEmailConfirmed] = useState(false)
 
     useEffect(()=>{
         console.log("Email: ", email)
@@ -13,6 +17,37 @@ const ForgotPasswordPage =()=>{
     return(
         <Container>
             <h2>Forgot Password Page</h2>
+             <InputField
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setEmail(e.target.value)
+                }
+            />
+            {isEmailConfirmed && (
+                <>
+                <InputField
+                    type="password"
+                    placeholder="New Password"
+                    value={newPassword}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        setNewPassword(e.target.value)
+                    }
+                />
+                
+                <InputField
+                    type="password"
+                    placeholder="Confirm New Password"
+                    value={confirmPassword}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        setConfirmPassword(e.target.value)
+                    }
+                />
+                </>
+            )
+            }
+            <Button onClick={()=>setEmailConfirmed(true)}>Submit</Button>
         </Container>
     )
 
