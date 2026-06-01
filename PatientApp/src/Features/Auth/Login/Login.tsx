f9db99546fe1b7f1e884a1707783b48eb076917d
SINGH Nitin <nitin.singh2@soprasteria.com>
2026-06-01 17:40:39 +0530

    work in progress

---
+++ PatientApp/src/Features/Auth/Login/Login.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { Container } from "./LoginStyle"
+
+const Login = () =>{
+return(
+    <Container>
+        Hello Nitin Prakash Singh. You are the best no one is like you here. you are honest, smart , hardworking and lucky.
+    </Container>
+)
+}
+export default Login
